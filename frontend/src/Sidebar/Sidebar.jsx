3ab91e6deef5a9633f385d4fdccd4424aa8ddbf1
thevinter <theniki98@gmail.com>
2021-11-04 23:51:11 +0100

    chore(release): fixed named import

--- frontend/src/Sidebar/Sidebar.jsx
+++ frontend/src/Sidebar/Sidebar.jsx
@@ -5,7 +5,7 @@ import React, { useState } from 'react';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import { Link } from 'react-router-dom';
-import { SidebarData } from './SidebarData';
+import SidebarData from './SidebarData';
 import './Sidebar.css';
 
 function Sidebar() {
