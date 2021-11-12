cbb13f32812fe5b08ce5651c77135a73a230e180
CallMeRush <a.russo016@studenti.unibs.it>
2021-11-13 00:15:20 +0100

    feat: navbar iniziata

--- src/AboutUs/AboutUs.jsx
+++ src/AboutUs/AboutUs.jsx
@@ -6,64 +6,31 @@ export default function AboutUs() {
     <div id="page">
       <h1 id="page-title">About Us</h1>
       <p className="text-justify">
-        <strong>IEEE Student Branch of Brescia</strong>
-        {' '}
-        is the reference for the
-        {' '}
-        <em>IEEE </em>
-        (Institute of Electrical and Electronics Engineers) in Brescia. Our
-        {' '}
-        <strong>main goal</strong>
-        {' '}
-        is to integrate the modus operandi of
-        scientific research into the academic and professional career of each
-        member, this takes place through group projects or individual research
-        in different fields of IT.
+        IEEE Student Branch of Brescia is the reference for the IEEE (Institute
+        of Electrical and Electronics Engineers) in Brescia. Our main goal is to
+        integrate the modus operandi of scientific research into the academic
+        and professional career of each member, this takes place through group
+        projects or individual research in different fields of IT.
       </p>
       <p className="text-justify">
-        The
-        {' '}
-        <strong>Branch</strong>
-        {' '}
-        offers the opportunity to
-        {' '}
-        <em>research</em>
-        {' '}
-        at the same level as IEEE members from around the world, while remaining
-        completely immersed in the academic environment, with the support of the
-        teaching staff and doctoral students.
+        The Branch offers the opportunity to research at the same level as IEEE
+        members from around the world, while remaining completely immersed in
+        the academic environment, with the support of the teaching staff and
+        doctoral students.
       </p>
       <p className="text-justify">
         The possibility to get in touch realities other than purely scholastic
-        allows one to have a different vision about what
-        {' '}
-        <strong>developing</strong>
-        {' '}
-        and
-        <strong>maintaining</strong>
-        {' '}
-        a project
-        mean, and also the ability to
-        <strong>learn</strong>
-        {' '}
-        new concepts.
+        allows one to have a different vision about what developing and
+        maintaining a project mean, and also the ability to learn new concepts.
       </p>
       <p className="text-justify">
-        In addition to the
-        {' '}
-        <em>projects</em>
-        , we give our members the
-        opportunity to attend a conference, to talk about their project and the
-        difficulties they have encountered, as well as the things they have
-        learned.
+        In addition to the projects, we give our members the opportunity to
+        attend a conference, to talk about their project and the difficulties
+        they have encountered, as well as the things they have learned.
       </p>
       <p className="text-justify">
-        Within the site there are some of the
-        {' '}
-        <em>projects</em>
-        {' '}
-        we are
-        developing or that we have carried out over the past year.
+        Within the site there are some of the projects we are developing or that
+        we have carried out over the past year.
       </p>
     </div>
   );
--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,93 +1,103 @@
-import React from 'react';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import MenuIcon from '@mui/icons-material/Menu';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+import './Navbar.css';
 import {
   Link, BrowserRouter as Router, Switch, Route,
 } from 'react-router-dom';
-import './Navbar.css';
-import Sidebar from '../Sidebar/Sidebar';
-import Login from '../Login/Login';
 import Projects from '../Projects/Projects';
 import AboutUs from '../AboutUs/AboutUs';
 
-import instagram from '../logos/logo-instagram.png';
-import linkedin from '../logos/logo-linkedin.png';
-import youtube from '../logos/logo-youtube.png';
-import facebook from '../logos/logo-facebook.png';
-import twitter from '../logos/logo-twitter.jpg';
-import github from '../logos/logo-github.png';
-
-export default function Navbar() {
+export default function ButtonAppBar() {
   return (
-    <Router>
-      <div id="Navbar">
-        <ul className="listaLink" id="Listasx">
-          <li>
-            <Sidebar />
-          </li>
-          <li className="scalable">
-            <Link to="." className="NavItem">
-              Home
-            </Link>
-          </li>
-          <li className="scalable">
-            <Link to="/projects" className="NavItem">
-              Progetti
-            </Link>
-          </li>
-          <li className="scalable">
-            <Link to="/aboutus" className="NavItem">
-              About Us
-            </Link>
-          </li>
-        </ul>
-        <ul className="listaLink" id="Listadx">
-          <li className="scalable">
-            <Link to="/login" className="NavItem">
-              Login
-            </Link>
-          </li>
-          <li className="scalable">
-            <a href="https://www.instagram.com/ieee_sb_brescia/">
-              <img className="logoSocial" src={instagram} alt="Instagram" />
-            </a>
-          </li>
-          <li className="scalable">
-            <a href="https://www.linkedin.com/company/stb1019">
-              <img className="logoSocial" src={linkedin} alt="LinkedIn" />
-            </a>
-          </li>
-          <li className="scalable">
-            <a href="https://www.youtube.com/channel/UCStUvurgwwAgs_W-k5Dci3Q">
-              <img className="logoSocial" src={youtube} alt="Youtube" />
-            </a>
-          </li>
-          <li className="scalable">
-            <a href="https://www.facebook.com/stb01019/">
-              <img className="logoSocial" src={facebook} alt="Facebook" />
-            </a>
-          </li>
-          <li className="scalable">
-            <a href="https://twitter.com/stb1019">
-              <img className="logoSocial" src={twitter} alt="Twitter" />
-            </a>
-          </li>
-          <li className="scalable">
-            <a href="https://github.com/STB1019">
-              <img className="logoSocial" src={github} alt="GitHub" />
-            </a>
-          </li>
-        </ul>
-      </div>
-      <Switch>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/projects">
-          <Projects />
-        </Route>
-        <Route path="/aboutus">
-          <AboutUs />
-        </Route>
-      </Switch>
-    </Router>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar sx={{ justifyContent: 'space-between' }}>
+          <div>
+            <IconButton edge="start">
+              <MenuIcon color="white" />
+            </IconButton>
+
+            <ButtonGroup variant="text" aria-label="text button group">
+              <Router>
+                <Link className="link" to="/">
+                  <Button className="sbutton" color="white">Home</Button>
+                </Link>
+                <Link className="link" to="/projects">
+                  <Button className="sbutton" color="white">Progetti</Button>
+                </Link>
+                <Link className="link" to="/aboutus">
+                  <Button className="sbutton" color="white">About Us</Button>
+                </Link>
+                <Switch>
+                  <Route path="/projects">
+                    <Projects />
+                  </Route>
+                  <Route path="/aboutus">
+                    <AboutUs />
+                  </Route>
+                </Switch>
+              </Router>
+            </ButtonGroup>
+          </div>
+          <ButtonGroup variant="text" aria-label="text button group">
+            <IconButton
+              edge="start"
+              sx={{ mr: 1 }}
+              href="https://www.instagram.com/ieee_sb_brescia/"
+            >
+              <InstagramIcon color="white" />
+            </IconButton>
+            <IconButton
+              edge="start"
+              sx={{ mr: 1 }}
+              href="https://www.linkedin.com/company/stb1019"
+            >
+              <LinkedInIcon color="white" />
+            </IconButton>
+            <IconButton
+              edge="start"
+              sx={{ mr: 1 }}
+              href="https://www.youtube.com/channel/UCStUvurgwwAgs_W-k5Dci3Q"
+            >
+              <YouTubeIcon color="white" />
+            </IconButton>
+            <IconButton
+              edge="start"
+              sx={{ mr: 1 }}
+              href="https://www.facebook.com/stb01019/"
+            >
+              <FacebookIcon color="white" />
+            </IconButton>
+            <IconButton
+              edge="start"
+              sx={{ mr: 1 }}
+              href="https://twitter.com/stb1019"
+            >
+              <TwitterIcon color="white" />
+            </IconButton>
+            <IconButton
+              edge="start"
+              sx={{ mr: 1 }}
+              href="https://github.com/STB1019"
+            >
+              <GitHubIcon color="white" />
+            </IconButton>
+          </ButtonGroup>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
