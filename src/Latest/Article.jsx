4434f7fd567cbfa40c615ff2ee780236da8e31ad
Alessio Russo <brittibaroni01@gmail.com>
2022-03-09 20:07:53 +0100

    fix: sistemazione latest

--- src/Latest/Article.jsx
+++ src/Latest/Article.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
+import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Casa from '../assets/prova.jpg';
 
 export default function Latest() {
+  const height = 380;
   return (
-    <Box sx={{
-      height: '500px', width: '500px', overflow: 'hidden',
-    }}
+    <Grid 
+      container
+      sx={{
+        width: '80%', height: '400px', mx: 'auto', my: '20px',
+      }}
+      backgroundColor="blue"
     >
-      <img src={Casa} alt="ccc" width="100%" backgroundSize="cover" fit="crop" auto="format" />
-    </Box>
+      <Grid 
+        item
+        xs={4}
+      >
+        <Box sx={{
+          width: '100%', height: `${height}px`, mx: '10px', my: '10px', overflow: 'hidden', backgroundImage: `url(${Casa})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center',
+        }}
+        />
+      </Grid>
+      <Grid item xs={8}>
+        <p>a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a</p>
+      </Grid>
+    </Grid>
   );
 }
