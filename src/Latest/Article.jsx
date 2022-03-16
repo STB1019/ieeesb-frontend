eac6299e76780db18962b3aa733d574c9c443b84
CallMeRush <a.russo016@studenti.unibs.it>
2022-03-16 19:30:26 +0100

    fix: inizio articolo con css

--- src/Latest/Article.jsx
+++ src/Latest/Article.jsx
@@ -1,27 +1,21 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import Casa from '../assets/prova.jpg';
+import './Article.css';
 
-export default function Latest() {
+export default function Article() {
   return (
     <Box
       className="whole"
+      style={{ backgroundImage: `url(${Casa})` }} 
     >
-      <Box
-        className="image-container"
-      >
-        <Box 
-          className="image"
-          sx={{
-            aspectRatio: '1', overflow: 'hidden', backgroundImage: `url(${Casa})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center',
-          }}
-        />
-      </Box>
-      <Box sx={{ width: '100px', backgroundColor: 'red' }}>
-        <p>
-          a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a
-          a a a a a a a a a a a a a
-        </p>
+      <Box className="image-container">
+        <Box className="image" style={{ backgroundImage: `url(${Casa})` }} />
+        <Box sx={{ width: '100px', backgroundColor: 'red' }}>
+          <p>
+            sheeeeeeeeeeeeeeeeeeeeeeeeeeesh
+          </p>
+        </Box>
       </Box>
     </Box>
   );
