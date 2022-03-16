a985b7e8fe6c0d2ea01a414c1d151e93a6ca0ccf
CallMeRush <a.russo016@studenti.unibs.it>
2022-03-16 18:36:57 +0100

    fix: funziona

--- src/Latest/Article.jsx
+++ src/Latest/Article.jsx
@@ -1,33 +1,24 @@
 import React from 'react';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Casa from '../assets/prova.jpg';
 
 export default function Latest() {
-  const height = 3;
   return (
     <Box
-      sx={{
-        width: `${width}px`, height: `${height}px`, display: 'flex', flexDirection: 'row', mx: 'auto', my: '20px',
-      }}
-      backgroundColor="blue"
+      className="whole"
     >
       <Box
-        sx={{
-          width: `${imgWidth}px`, height: `${imgHeight}px`, px: `${imgPadding}px`, py: `${imgPadding}px`,
-        }}
+        className="image-container"
       >
-        <Box sx={{
-          width: '100%', height: '100%', overflow: 'hidden', backgroundImage: `url(${Casa})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center',
-        }}
+        <Box 
+          className="image"
+          sx={{
+            aspectRatio: '1', overflow: 'hidden', backgroundImage: `url(${Casa})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center',
+          }}
         />
       </Box>
-      <Box sx={{ width: `${textWidth}px`, backgroundColor: 'red' }}>
+      <Box sx={{ width: '100px', backgroundColor: 'red' }}>
         <p>
-          {window.innerWidth}
-          {width}
-          {a}
-          {stateVariable}
           a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a
           a a a a a a a a a a a a a
         </p>
