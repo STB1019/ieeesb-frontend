2f12a11dea02a58856405a14ebb9d32f1f3f1cf1
Alessio Russo <a.russo016@studenti.unibs.it>
2022-03-26 21:57:55 +0100

    fix: aggiunta file article

---
+++ src/article/Article.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import './Article.css';
+
+export default function Article() {
+  return (
+    <Box>
+      <p>a</p>
+    </Box>
+  );
+}
